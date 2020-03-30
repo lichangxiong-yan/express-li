528b9e1cd96afaea0aa0095dd786589ed8ce2008
lichangxiong <2689211674@qq.com>
2020-03-30 19:23:12 +0800

    静态页面

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.use("/posts",postRouter)
 
 // 统一错误处理
 app.use( (err,req,res,next)=>{
+
+  //可以将错误的信息写入到某个文件中，方便后续去查看文件
+  //fs 模块  fs.writeFile
+  //      不能使用  fs.writeFile 要用  fs.appendFile   因为这个是全量去写 每次写都会吧前面的覆盖掉
+      //  要用  appendFile  是在某个文件中去增加 不会覆盖
   console.error(err)
   res.status(500).send(err.message)
 })
